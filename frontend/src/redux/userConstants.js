9428df32bfcbb24bcbcc1e7835b47396b1ffaf18
Adibros <yadityashastri@gmail.com>
2023-02-16 06:01:05 +0530

    setting up redux structure

---
+++ frontend/src/redux/userConstants.js
@@ -0,0 +1,7 @@
+// all the constants just to keep in one place
+
+export const USER_LOGIN_REQUEST = "USER_LOGIN_REQUEST";
+export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+export const USER_LOGIN_FAIL = "USER_LOGIN_FAIL";
+export const USER_LOGOUT = "USER_LOGOUT";
+
